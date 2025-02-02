0bf95e088e4de1e44f66430424534da02164c3d9
root <root@DESKTOP-AK3BBH4>
2025-02-02 10:28:11 +0100

    wiwi

--- main.c
+++ main.c
@@ -24,25 +24,17 @@ int main(int ac, char **av)
 {
     char    *joined_arguments;
     int     i;
-    char    **split;
 
     if (!av || check_input(ac, av) == 0)
         exit_error(NULL, NULL);
-    
+    if (ac < 2)
+	    exit (1);
     if (ac == 2)
-    {
-        joined_arguments = ft_strdup(av[1]);
-        if (!joined_arguments)
-            exit_error(NULL, NULL);
-    }
+	    joined_arguments = ft_strdup(av[1]);
     else
-    {
-        joined_arguments = join_arguments(ac, av);
-        if (!joined_arguments)
-            exit_error(NULL, NULL);
-    }
-	if (check_numbers(joined_arguments) == 0)
-		error_free(joined_arguments);
+	    joined_arguments = join_arguments(ac, av);
+    //if (check_numbers(joined_arguments) == 0)
+//		error_free(joined_arguments);
 	write (1, "good", 4);
     free(joined_arguments);
 	return (0);
