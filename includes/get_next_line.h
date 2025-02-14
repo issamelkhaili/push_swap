dcae0231e0fe8a3e6cda9b54dd1d3e5fcb51b1f7
wsl <elkhailiissam0@gmail.com>
2025-02-14 14:04:11 +0100

    fix norm

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -6,11 +6,7 @@
 /*   By: jdecorte <jdecorte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 11:15:27 by jdecorte          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/02/14 13:44:16 by isel-kha         ###   ########.fr       */
-=======
-/*   Updated: 2025/02/14 12:01:07 by isel-kha         ###   ########.fr       */
->>>>>>> 35563cd3723288b54afd735a251db03604aa4d55
+/*   Updated: 2025/02/14 14:03:59 by isel-kha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +19,6 @@
 #  define BUFFER_SIZE 42
 # endif
 
-<<<<<<< HEAD
-=======
-// Function prototypes for get_next_line specific functions
->>>>>>> 35563cd3723288b54afd735a251db03604aa4d55
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *string, int searchedChar);
 void	ft_bzero(void *s, size_t n);
